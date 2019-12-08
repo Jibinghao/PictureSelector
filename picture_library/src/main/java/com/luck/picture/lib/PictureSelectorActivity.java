9617cf11bb894f4a2d3291697cddaad484fee9c4
jibinghao <769499597@qq.com>
2019-12-08 17:53:18 +0800

    更新到2.3.9

--- picture_library/src/main/java/com/luck/picture/lib/PictureSelectorActivity.java
+++ picture_library/src/main/java/com/luck/picture/lib/PictureSelectorActivity.java
@@ -486,7 +486,7 @@ public class PictureSelectorActivity extends PictureBaseActivity implements View
         boolean eqVideo = PictureMimeType.eqVideo(mimeType);
         if (eqVideo) {
             if (image.getDuration() > 16 * 1000) {
-                ToastUtils.s(getContext(), "请选择不超过15秒的视频");
+                ToastUtils.s(getContext(), "视频不能超过15秒");
                 return;
             }
         }
